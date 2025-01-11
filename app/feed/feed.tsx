e9b634283db1c39a6921f560d78d1abdf726d0dc
parkcool <yickle1345@naver.com>
2025-01-11 03:36:56 +0000

    Chore: Install npm packages and remove unnecessary files

--- app/feed/feed.tsx
+++
@@ -1,3 +0,0 @@
-export function Feed() {
-  return <div></div>;
-}
--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/feed";
-import { Feed } from "../feed/feed";
+import Feed from "~/pages/feed";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "모먼티" }];
 }
 
-export default function Component() {
+export default function Component({}: Route.ComponentProps) {
   return <Feed />;
 }
