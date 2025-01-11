63efc013614445d7b3459b4f6acbd7221203c63f
parkcool <yickle1345@naver.com>
2025-01-11 08:17:46 +0000

    Feat: Add `Presesable` component

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,24 +1,50 @@
-import { styled } from "styled-components";
+import { useContext } from "react";
+import { styled, ThemeContext } from "styled-components";
 
+import { MdMenu } from "react-icons/md";
+
+import Pressable from "~/components/Pressable";
 import Logo from "~/assets/svg/logo.svg?react";
 
 const StyledHeader = styled.header`
   display: flex;
-  padding: 30px 20px;
+  padding: 20px 20px;
   justify-content: space-between;
+  align-items: center;
   position: sticky;
   top: 0;
   background: ${(props) => props.theme.bg1};
 `;
 
+const FullWidthDiv = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const StyledPressable = styled(Pressable)`
+  padding: 5px;
+  border-radius: 50%;
+`;
+
 export default function AppBar() {
+  const theme = useContext(ThemeContext);
+
   return (
     <StyledHeader>
-      <div></div>
-      <div>
+      {/* left */}
+      <FullWidthDiv style={{ justifyContent: "flex-start" }}>
+        <StyledPressable backgroundColor={theme?.bg1}>
+          <MdMenu size="36" color={theme?.grey1} />
+        </StyledPressable>
+      </FullWidthDiv>
+
+      {/* center */}
+      <FullWidthDiv style={{ justifyContent: "center" }}>
         <Logo width="60" height="100%" />
-      </div>
-      <div></div>
+      </FullWidthDiv>
+
+      {/* right */}
+      <FullWidthDiv style={{ justifyContent: "flex-end" }}></FullWidthDiv>
     </StyledHeader>
   );
 }
---
+++ app/components/Pressable.tsx
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+
+import isDarkColor from "~/utils/isDarkColor";
+
+const Wrapper = styled.button<{ $background: string }>`
+  padding: 0;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: ${(props) => props.$background};
+  transition: filter 0.2s, transform 0.1s ease-in-out;
+  -webkit-tap-highlight-color: transparent;
+
+  &:focus {
+    transform: scale(1.05);
+    filter: brightness(
+      ${(props) => (isDarkColor(props.$background) ? "110%" : "90%")}
+    );
+  }
+
+  &:active {
+    transform: scale(0.95);
+    filter: brightness(
+      ${(props) => (isDarkColor(props.$background) ? "120%" : "80%")}
+    );
+  }
+`;
+
+const Content = styled.div``;
+
+interface PressableProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  backgroundColor?: string;
+}
+
+export default function Pressable({
+  children,
+  backgroundColor = "#0000000",
+  ...props
+}: PressableProps) {
+  return (
+    <Wrapper {...props} $background={backgroundColor}>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+}
