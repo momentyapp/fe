7e3c82f3e4e89326a43f63f66222e4c8e5bb9d71
parkcool <yickle1345@naver.com>
2025-01-28 07:44:58 +0000

    Feat: Fetch trending topics from API and update state in Feed component

--- app/pages/feed.tsx
+++ app/pages/feed.tsx
@@ -8,6 +8,8 @@ import TopicFilter from "~/components/TopicFilter";
 import MomentContainer from "~/components/MomentContainer";
 import Pressable from "~/components/Pressable";
 
+import API from "~/apis";
+
 import type { Moment as MomentType, Topic } from "common";
 
 const FloatingButton = styled(Pressable)`
@@ -21,64 +23,32 @@ const FloatingButton = styled(Pressable)`
   align-items: center;
 `;
 
-const sampleTopics: Topic[] = [
-  { topic: "매일우유", id: 0, enabled: false },
-  { topic: "국회의사당역", id: 1, enabled: false },
-  { topic: "탄핵", id: 2, enabled: false },
-];
-
-const sampleMoments: MomentType[] = [
-  {
-    id: 1,
-    author: {
-      id: 1,
-      username: "사용자",
-      createdAt: "2023-01-01T00:00:00Z",
-      photo: "https://picsum.photos/200",
-    },
-    createdAt: "2023-10-01T12:00:00Z",
-    body: {
-      text: `식품의약품안전처는 이미 및 이취 등이 감지된 매일우유 제품을 판매 중단하고 회수한다고 오늘(13일) 밝혔습니다.
-
-대상 제품은 매일유업 광주 공장에서 제조한 ‘메일우유’이며, 소비 기한은 내년 2월 16일입니다.`,
-      photos: ["https://picsum.photos/400/200", "https://picsum.photos/350"],
-    },
-    topics: [
-      { topic: "매일우유", id: 0 },
-      { topic: "코로나19", id: 4 },
-    ],
-    reactions: {
-      "🤣": 10,
-      "👍": 5,
-    },
-    expiresAt: "2023-10-02T12:00:00Z",
-    myEmoji: "👍",
-  },
-  {
-    id: 2,
-    createdAt: "2023-10-02T12:00:00Z",
-    body: {
-      text: "두 번째 게시글입니다. 😉",
-    },
-    topics: [
-      { topic: "국회의사당역", id: 1 },
-      { topic: "백신", id: 5 },
-    ],
-    reactions: {
-      "😉": 20,
-      "👏": 3,
-    },
-  },
-];
-
 export default function Feed() {
   const navigate = useNavigate();
   const location = useLocation();
 
   const theme = useContext(ThemeContext);
 
-  const [topics, setTopics] = useState<Topic[]>(sampleTopics);
-  const [moments, setMoments] = useState<MomentType[]>(sampleMoments);
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [moments, setMoments] = useState<MomentType[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const trendingTopics = await API.topic.getTrendingTopics();
+      const { code, message, result } = trendingTopics.data;
+
+      if (code === "success" && result !== undefined) {
+        setTopics(
+          result.topics.map((topic) => ({
+            id: topic.id,
+            topic: topic.name,
+            trending: topic.trending,
+            count: topic.usage,
+          }))
+        );
+      }
+    })();
+  }, []);
 
   function handleWrite() {
     navigate("/write");
