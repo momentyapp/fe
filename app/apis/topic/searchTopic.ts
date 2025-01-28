e649f078d620c16da3d5876bd1b58d9f27824fb4
parkcool <yickle1345@naver.com>
2025-01-28 07:18:57 +0000

    Feat: Implement `searchTopic` function for topic search API

---
+++ app/apis/topic/searchTopic.ts
@@ -0,0 +1,23 @@
+import axios from "~/apis";
+import type { ApiResponse } from "common";
+
+interface Props {
+  query: string;
+}
+
+type Response = ApiResponse<{
+  count: number;
+  topics: {
+    id: number;
+    name: string;
+    trending?: boolean;
+    usage?: number;
+  }[];
+}>;
+
+export default async function searchTopic({ query }: Props) {
+  const result = await axios.get<Response>("/topics/search", {
+    params: { query },
+  });
+  return result;
+}
