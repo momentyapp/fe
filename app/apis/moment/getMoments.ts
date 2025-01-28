63e4522e786dba4a43ce327d7be7de689ed47253
parkcool <yickle1345@naver.com>
2025-01-28 07:28:52 +0000

    Feat: Implement `getMoments` function for fetching moments by topic IDs

---
+++ app/apis/moment/getMoments.ts
@@ -0,0 +1,20 @@
+import axios from "~/apis";
+import type { ApiResponse, Moment } from "common";
+
+interface Props {
+  topicIds: number[];
+  before?: number;
+}
+
+type Response = ApiResponse<{
+  count: number;
+  moments: Moment[];
+}>;
+
+export default async function getMoments({ topicIds, before }: Props) {
+  const result = await axios.post<Response>("/moment", {
+    topicIds,
+    before,
+  });
+  return result;
+}
