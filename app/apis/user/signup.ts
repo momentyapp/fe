6dc55b81ef94043d5b8f8f34cfe6c7c92a9d0824
parkcool <yickle1345@naver.com>
2025-01-28 07:22:15 +0000

    Feat: Implement `signup` function for user registration API

---
+++ app/apis/user/signup.ts
@@ -0,0 +1,24 @@
+import axios from "~/apis";
+import type { ApiResponse } from "common";
+
+interface Props {
+  username: string;
+  password: string;
+  photo?: File;
+}
+
+type Response = ApiResponse;
+
+export default async function signup({ username, password, photo }: Props) {
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
+  if (photo) formData.append("photo", photo);
+
+  const result = await axios.post<Response>("/user", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return result;
+}
